33a3391354ec9a0dceab0e329a91f4a249a3a795
Kamil Dudkowski <kamil.dudkowski@gmail.com>
2026-06-07 05:33:49 +0200

    Czysta, płaska struktura rdzenia

---
+++ assets/js/google-login.js
@@ -0,0 +1,2 @@
+// Uniwersalny skrypt PPHPC do obsługi logowania Google
+console.log("PPHPC Google Login Asset Loaded.");
---
+++ src/CoreTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace PPHPC;
+
+class CoreTest 
+{
+    public function info(): string 
+    {
+        return "Silnik PPHPC zainicjalizowany poprawnie!";
+    }
+}
+?>
